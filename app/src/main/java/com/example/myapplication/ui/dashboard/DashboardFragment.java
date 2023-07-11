e6b13eb5cfed0fd647432366997f64cccbc240bf
Zeubi13 <94932120+Zeubi13@users.noreply.github.com>
2023-07-11 09:18:09 +0200

    Homefragment is nice with rating

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.java
@@ -1,9 +1,12 @@
 package com.example.myapplication.ui.dashboard;
 
+
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
 import android.widget.AdapterView;
 import android.widget.TextView;
 import android.widget.Toast;
