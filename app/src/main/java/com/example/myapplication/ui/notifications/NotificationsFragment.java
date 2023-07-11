84036423dbfdd7cdc0934625925c1da6c1670fa1
Zeubi13 <94932120+Zeubi13@users.noreply.github.com>
2023-07-12 00:51:04 +0200

    Note Done 1h00

--- app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/myapplication/ui/notifications/NotificationsFragment.java
@@ -4,8 +4,23 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -14,9 +29,17 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.myapplication.R;
 import com.example.myapplication.databinding.FragmentNotificationsBinding;
 
+import java.io.OutputStreamWriter;
+
 public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
+    private Button Button;
+    private EditText EditText;
+
+
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         NotificationsViewModel notificationsViewModel = new ViewModelProvider(this).get(NotificationsViewModel.class);
@@ -24,13 +47,58 @@ public class NotificationsFragment extends Fragment {
         binding = FragmentNotificationsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final ImageView imageView = binding.save;
-        imageView.setImageResource(R.drawable.ic_save);
+        String content = Open(requireContext(), "Note.txt");
+
+
+        EditText = (EditText) root.findViewById(R.id.EditText);
+
+        EditText.setText(content);
+
+        Button = (Button) root.findViewById(R.id.save);
+        this.Button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Save(requireContext(),"Note.txt");
+            }
+        });
 
         final TextView textView = binding.textNotifications;
         notificationsViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
+
     }
+
+    public String Open(Context context, String fileName) {
+        String content = "";
+            try {
+                InputStream in = context.openFileInput(fileName);
+                if ( in != null) {
+                    InputStreamReader tmp = new InputStreamReader( in );
+                    BufferedReader reader = new BufferedReader(tmp);
+                    String str;
+                    StringBuilder buf = new StringBuilder();
+                    while ((str = reader.readLine()) != null) {
+                        buf.append(str + "\n");
+                    } in .close();
+                    content = buf.toString();
+                }
+            } catch (java.io.FileNotFoundException e) {} catch (Throwable t) {
+                Toast.makeText(context, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+            }
+        return content;
+    }
+
+    public void Save(Context context, String fileName) {
+        try {
+            FileOutputStream fileOutputStream = context.openFileOutput(fileName, Context.MODE_PRIVATE);
+            OutputStreamWriter out = new OutputStreamWriter(fileOutputStream);
+            out.write(EditText.getText().toString());
+            out.close();
+            Toast.makeText(context, "Note Saved!", Toast.LENGTH_SHORT).show();
+        } catch (Throwable t) {
+            Toast.makeText(context, "Exception: " + t.toString(), Toast.LENGTH_LONG).show();
+        }
+    }
+
 
     @Override
     public void onDestroyView() {
